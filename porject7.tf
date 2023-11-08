de85b28f72ddd2913790909cb31b910d481caccb
saik8712 <139668888+saik8712@users.noreply.github.com>
2023-11-08 14:43:53 +0530

    Update porject7.tf

--- porject7.tf
+++ porject7.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-region = "us-east-1"
+region = "us-west-2"
 access_key = "AKIA55O2WA5A6XKDED6Y"
 secret_key = "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"
 }
-resource "aws_s3_bucket" "s3bucket" {
- bucket = "bhagi-terra123"
+resource "aws_s3_bucket" "sais3bucket" {
+ bucket = "sai1-terra123"
  tags = {
  Name = "terraform"
  }
